5ddd896dc67443c92e279c4e66a1dca85a7132b1
Nitrous <n20gaming2000@gmail.com>
2017-04-25 14:52:58 +0800

    Initialization

---
+++ conf.lua
@@ -0,0 +1,14 @@
+function love.conf(t)
+	t.identity 	= "SIF Icon Builder"
+	t.version  	= "0.10.2"
+	t.console	= true
+	
+	t.window.title 	= "Idol Icon Builder"
+	t.window.icon	= nil
+	t.window.width	= 550
+	t.window.height = 390
+	t.window.msaa = 16
+	t.window.borderless	= false
+	t.window.resizeable	= false
+	t.window.fullscreen = false
+end
